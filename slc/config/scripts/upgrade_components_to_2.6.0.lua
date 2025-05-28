9f99e4f61d846376f06fbc3a6313bf8092922791
Bhavani Manda <Bhavani.Manda@silabs.com>
2025-05-28 09:52:00 +0000

    Pull request #462: MATTER-4506: Removed wifi_resources from matter
    Merge in WMN_TOOLS/matter_extension from remove/wifi_resources_component to release_2.6-1.4
    
    Squashed commit of the following:
    
    commit 5d06fc6fa29308dc4c7071e4db6e64f4a5022dad
    Author: chirag-silabs <chirag.bansal@silabs.com>
    Date:   Wed May 28 00:08:31 2025 +0530
    
        merging the condition in lua file
    
    commit a2b29c87b0d75fab03b47dffc12231c0e7b39000
    Author: bhmanda <bhmanda@silabs.com>
    Date:   Tue May 27 21:01:46 2025 +0530
    
        Updated lua script to remove matter component and add wiseconnect wifi_resources component
    
    commit 1701cf65a54913ba62d747579a7b6a82a5e541ac
    Author: bhmanda <bhmanda@silabs.com>
    Date:   Tue May 27 18:32:06 2025 +0530
    
        Removed all the files which are wifi_resources component
    
    commit 2334cab76b30afee5208a937296f6c62d8a442ea
    Author: bhmanda <bhmanda@silabs.com>
    Date:   Tue May 27 15:33:08 2025 +0530
    
        Removed wifi_resources component and replaced with wiseconnect sdk. Changes done accordingly in slcp files.

--- slc/config/scripts/upgrade_components_to_2.6.0.lua
+++ slc/config/scripts/upgrade_components_to_2.6.0.lua
@@ -18,8 +18,15 @@ if slc.is_selected('matter_gatt') then
     })
   end
 end
-
 if slc.is_selected('matter_platform_siwx917') or slc.is_selected('siwx917_ncp') then
+  table.insert(changeset, {
+    ['component'] = '%extension-matter%wifi_resources',
+    ['action'] = 'remove'
+  })
+  table.insert(changeset, {
+    ['component'] = '%extension-wiseconnect3_sdk%wifi_resources',
+    ['action'] = 'add'
+  })
   table.insert(changeset, {
     ['component'] = '%extension-wiseconnect3_sdk%sl_si91x_lwip_stack',
     ['action'] = 'remove'
