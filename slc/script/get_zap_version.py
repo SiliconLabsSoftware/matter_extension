66f1e5704269743e653b41d5397fc611308d7a4e
Sarthak Shaha <sarthak.shaha@silabs.com>
2025-06-03 11:42:05 -0400

    use latest zap

--- slc/script/get_zap_version.py
+++ slc/script/get_zap_version.py
@@ -1,13 +1,6 @@
-"""
-This script is used to determine the latest ZAP version from a JSON configuration file from matter_sdk.
-
-Usage:
-    Run the script directly to print the minimum zap level required:
-    $ python get_zap_version.py
-
-"""
 import os
 import json
+import argparse
 from pathlib import Path
 
 # This string should remain empty on the main branch. Update it only when a release branch is created. 
@@ -21,11 +14,9 @@ def get_zap_version():
     # Check if the environment variable SILABS_MATTER_ROOT is set
     if "SILABS_MATTER_ROOT" not in os.environ:
         # If not set, use the default path for Matter root
-        print("Using default path for Matter root")
         silabs_chip_root = Path(__file__).resolve().parents[2]
     else:
         # If set, use the path from the environment variable
-        print("Using ENV path for Matter root")
         silabs_chip_root = os.environ["SILABS_MATTER_ROOT"]
 
     zap_version = ""
@@ -50,5 +41,12 @@ def get_zap_version():
                 return zap_version
 
 if __name__ == "__main__":
-    # Print the minimum zap required
-    print("The minimum zap required is", get_zap_version())
+    parser = argparse.ArgumentParser(description="Get ZAP version")
+    parser.add_argument('--docker', action='store_true', help='Indicate running in Docker')
+    args = parser.parse_args()
+
+    zap_version = get_zap_version()
+    if args.docker:
+        print(zap_version)
+    else:
+        print("The minimum zap required is", zap_version)
