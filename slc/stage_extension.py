b14379e82e1b9be2dc4ae7f2e5b5487cda637ac6
Chirag Bansal <Chirag.Bansal@silabs.com>
2025-07-29 02:37:26 +0000

    Pull request #560: Updating the bleeding edge to latest main
    Merge in WMN_TOOLS/matter_extension from update/main_to_release_2.6-1.4 to main
    
    Squashed commit of the following:
    
    commit a02a1380835f23c5f2719f2aa438a7ea7b91347d
    Merge: 1857223b 823e65cb
    Author: chirag-silabs <chirag.bansal@silabs.com>
    Date:   Mon Jul 28 21:56:32 2025 +0530
    
        Merge branch 'main' of ssh://stash.silabs.com/wmn_tools/matter_extension into update/main_to_release_2.6-1.4
    
    commit 1857223bbafb2803d438cf5a4e00abe10b75f102
    Author: chirag-silabs <chirag.bansal@silabs.com>
    Date:   Fri Jul 25 22:09:27 2025 +0530
    
        fixing the package name
    
    commit e38a5759ce9afbfc167f81a39221f9b72ee0ec0a
    Author: chirag-silabs <chirag.bansal@silabs.com>
    Date:   Fri Jul 25 21:08:30 2025 +0530
    
        correcting the component for the nerge
    
    commit ad1350658fe1d8ebd96a089a517f42a2ccafa710
    Author: chirag-silabs <chirag.bansal@silabs.com>
    Date:   Fri Jul 25 19:58:53 2025 +0530
    
        updating the sdk versions for closure app
    
    commit 1f83b79ebc9fdfeee4138406ec417384307571b9
    Merge: 510c6a0b 2f870b32
    Author: chirag-silabs <chirag.bansal@silabs.com>
    Date:   Fri Jul 25 19:17:10 2025 +0530
    
        Merge remote-tracking branch 'origin/main' into update/main_to_release_2.6-1.4
    
    commit 510c6a0b3f58e82ff00a34bca01fd009f6ab50a1
    Author: chirag-silabs <chirag.bansal@silabs.com>
    Date:   Thu Jul 24 19:58:06 2025 +0530
    
        Adding the lwip component for the 917NCP
    
    commit cf54c20920dc9ef21346238ca82550d1050867f3
    Author: chirag-silabs <chirag.bansal@silabs.com>
    Date:   Thu Jul 24 18:50:56 2025 +0530
    
        Updating the matter private and matter sdk pointers
    
    commit 22ce065f0a6aea81a66258f104b6ca955221cc66
    Author: chirag-silabs <chirag.bansal@silabs.com>
    Date:   Wed Jul 23 14:41:43 2025 +0530
    
        zigbee app fixes and openthread renaming
    
    commit f3e96747142d01bc0d54535fdf81ed656d290f4d
    Author: Sarthak Shaha <Sarthak.Shaha@silabs.com>
    Date:   Tue Jul 22 15:25:44 2025 +0000
    
        Pull request #572: NOJIRA: update stage_extension to remove long paths
    
        Merge in WMN_TOOLS/matter_extension from fix_long_path to update/main_to_release_2.6-1.4
    
        Squashed commit of the following:
    
        commit a75860ab948d70263c00bd3c15ddf9a01c81d019
        Author: Sarthak Shaha <sarthak.shaha@silabs.com>
        Date:   Tue Jul 22 10:46:38 2025 -0400
    
            update stage_extension to remove long paths
    
    commit b608b28979cdf470e12b3f489a0d5f2b8808158e
    Author: chirag-silabs <chirag.bansal@silabs.com>
    Date:   Tue Jul 22 18:45:04 2025 +0530
    
        fixing the package and performance app
    
    commit b1509604c44fad6976606be7989b1972d4c2a7f4
    Author: chirag-silabs <chirag.bansal@silabs.com>
    Date:   Tue Jul 22 00:47:30 2025 +0530
    
        zigbee app fix and bleeding to latest
    
    commit 0e67c8a19f028733a05a7c1d225b9ff26409fa62
    Author: chirag-silabs <chirag.bansal@silabs.com>
    Date:   Tue Jul 22 00:41:14 2025 +0530
    
        bleeding edge update with running the script

--- slc/stage_extension.py
+++ slc/stage_extension.py
@@ -27,7 +27,7 @@ exclude_root_directories = [ "matter_extension/out", "matter_extension/slc/tools
 exlude_submodules = ['simplicity_sdk', 'wifi_sdk', "matter_private", "third_party_hw_drivers_extension"]
 
 #specific exlude from matter_sdk
-matter_sdk_exclude = ['matter_sdk/third_party','matter_sdk/zzz_generated','matter_sdk/examples/virtual-device-app','matter_sdk/src/controller/java/generated']
+matter_sdk_exclude = ['matter_sdk/third_party','matter_sdk/zzz_generated','matter_sdk/examples/virtual-device-app','matter_sdk/src/controller/java/generated','matter_sdk/examples/chef/devices','matter_sdk/scripts/py_matter_idl/matter/idl/tests/outputs/large_all_clusters_app']
 
 # Specific includes for wiseconnect_wifi_bt_sdk
 wiseconnect_wifi_bt_sdk_includes = ['sapi']
