7e98483978e3f8cbd24716f95132115be5909bc9
Mathieu Kardous <mathieu.kardous@silabs.com>
2025-10-03 11:44:25 -0400

    Update provisioning sources to match latest changes

--- provision/modules/jlink.py
+++ provision/modules/jlink.py
@@ -40,10 +40,6 @@ class JLinkChannel(_base.Channel):
             print("* Open DEFAULT connection to {}\n".format(self.part_number))
             self.link.open()
 
-        # This line is not in the provisioning repository.
-        # This is a workaround to enable SQA to use the local JLinkDevice instead of the one in the JLink installation folder.
-        # Do not remove this until SQA updates their test setups.
-        self.link.exec_command("JLinkDevicesXMLPath {}/".format(self.support_dir))
         self.link.set_tif(interface=pylink.JLinkInterfaces.SWD)
         self.link.connect(chip_name=self.part_number, speed="auto", verbose=True)
         # Use custom RTT control block address, if configured
@@ -65,14 +61,17 @@ class JLinkChannel(_base.Channel):
         # print("SENT({})".format(sent))
         return sent
 
-    def read(self):
+    def read(self, timeout_s=2):
         # print("Read...")
         chunk = []
         data = []
+        start_time = time.time()
         while (len(chunk) > 0) or (0 == len(data)):
             chunk = self.link.rtt_read(0, 1024)
-            data.extend(chunk)
-        # print("RECEIVED({}): {}".format(len(data), data))
+            if chunk:
+                data.extend(chunk)
+            elif (time.time() - start_time) > timeout_s:
+                return None
         return bytes(data)
 
     def reset(self, do_halt=False):
