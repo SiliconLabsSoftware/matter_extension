a00b0d5fad2c4d665aee1d58e825f47bde3e6d08
Matthew Albert <matthew.albert@silabs.com>
2025-10-15 15:56:06 -0400

    Slc defined config

---
+++ slc/inc/platform/silabs/tracing/SilabsTracingConfig.h
@@ -0,0 +1,17 @@
+
+#ifndef PLATFORM_SILABS_TRACING_SILABSTRACINGCONFIG_H_
+#define PLATFORM_SILABS_TRACING_SILABSTRACINGCONFIG_H_
+
+// <<< Use Configuration Wizard in Context Menu >>>
+
+// <q TRACING_RUNTIME_STATS> Collect and report runtime statistics
+// <i> Default: 0
+#define TRACING_RUNTIME_STATS 0
+
+// <q TRACING_RUNTIME_STATS_MAX_TASKS> Maximum number of tasks to report
+// <i> Default: 16
+#define TRACING_RUNTIME_STATS_MAX_TASKS 16
+
+// <<< end of configuration section >>>
+
+#endif // PLATFORM_SILABS_TRACING_SILABSTRACINGCONFIG_H_
