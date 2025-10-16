0064899aab7f975cbcf4fd469ec476fb904e0caf
Matthew Albert <matthew.albert@silabs.com>
2025-10-16 17:35:14 -0400

    update descriptions

--- slc/inc/platform/silabs/tracing/SilabsTracingConfig.h
+++ slc/inc/platform/silabs/tracing/SilabsTracingConfig.h
@@ -4,15 +4,15 @@
 
 // <<< Use Configuration Wizard in Context Menu >>>
 
-// <q TRACING_RUNTIME_STATS> Collect and report runtime statistics
+// <q TRACING_RUNTIME_STATS> Enable FreeRTOS runtime statistics collection
 // <i> Default: 0
 #define TRACING_RUNTIME_STATS 0
 
-// <q TRACING_RUNTIME_STATS_MAX_TASKS> Maximum number of tasks to report
+// <q TRACING_RUNTIME_STATS_MAX_TASKS> Maximum number of tasks to track for runtime statistics.
 // <i> Default: 16
 #define TRACING_RUNTIME_STATS_MAX_TASKS 16
 
-// <q TRACING_TIME_TRACKER_STORAGE_SIZE> Maximum number of tasks to report
+// <q TRACING_TIME_TRACKER_STORAGE_SIZE> Size in bytes for serialized time tracker storage.
 // <i> Default: 512
 #define TRACING_TIME_TRACKER_STORAGE_SIZE 512
 
