8994fb3157c225e7a64eac2c1fa14a748f75d548
Sarthak Shaha <sarthak.shaha@silabs.com>
2025-11-11 14:46:28 -0500

    reduce gen timeout

--- slc/build.sh
+++ slc/build.sh
@@ -318,7 +318,7 @@ if [ "$skip_gen" = false ]; then
 
 			# Generate bootloader
 			echo "Generating bootloader..."
-			run_slc_generate_with_retry generate --tt -s $GSDK_ROOT --daemon -d $OUTPUT_DIR $PROJECT_FLAG $SILABS_APP_PATH $BOOTLOADER_WITH_ARG $BOOTLOADER_WITHOUT_ARG -pids bootloader $CONFIG_ARGS --generator-timeout=3500
+			run_slc_generate_with_retry generate --tt -s $GSDK_ROOT --daemon -d $OUTPUT_DIR $PROJECT_FLAG $SILABS_APP_PATH $BOOTLOADER_WITH_ARG $BOOTLOADER_WITHOUT_ARG -pids bootloader $CONFIG_ARGS --generator-timeout=180
 			if [ $? -ne 0 ]; then
 				echo "FAILED TO Generate bootloader for: $SILABS_APP_PATH"
 				exit 1
@@ -331,7 +331,7 @@ if [ "$skip_gen" = false ]; then
 			APP_WITHOUT_ARG=$(build_without_arg "$WITHOUT_APP_COMPONENTS")
 
 			echo "Generating application..."
-			run_slc_generate_with_retry generate --tt -s $GSDK_ROOT --daemon -d $OUTPUT_DIR $PROJECT_FLAG $SILABS_APP_PATH $APP_WITH_ARG $APP_WITHOUT_ARG -pids application $CONFIG_ARGS --generator-timeout=3500
+			run_slc_generate_with_retry generate --tt -s $GSDK_ROOT --daemon -d $OUTPUT_DIR $PROJECT_FLAG $SILABS_APP_PATH $APP_WITH_ARG $APP_WITHOUT_ARG -pids application $CONFIG_ARGS --generator-timeout=180
 			if [ $? -ne 0 ]; then
 				echo "FAILED TO Generate application for: $SILABS_APP_PATH"
 				exit 1
@@ -339,7 +339,7 @@ if [ "$skip_gen" = false ]; then
 		fi
 	else
 		# Generate .slcp projects
-		run_slc_generate_with_retry generate -d $OUTPUT_DIR $PROJECT_FLAG $SILABS_APP_PATH --with $SILABS_BOARD $CONFIG_ARGS --generator-timeout=3500 -o makefile
+		run_slc_generate_with_retry generate -d $OUTPUT_DIR $PROJECT_FLAG $SILABS_APP_PATH --with $SILABS_BOARD $CONFIG_ARGS --generator-timeout=180 -o makefile
 		if [ $? -ne 0 ]; then
 			echo "FAILED TO Generate : $SILABS_APP_PATH"
 			exit 1
