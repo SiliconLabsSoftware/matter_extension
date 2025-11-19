fe7038a1b921843ba88a43f013148175817acadd
Matthew Albert <matthew.albert@silabs.com>
2025-11-19 09:28:06 -0500

    Rename defines

--- slc/inc/platform/silabs/tracing/SilabsTracingConfig.h
+++ slc/inc/platform/silabs/tracing/SilabsTracingConfig.h
@@ -11,17 +11,17 @@
 // <i> Default: 16
 #define TRACING_RUNTIME_STATS_MAX_TASKS 16
 
-// <q SILABS_TRACING_ENERGY_STATS> Enable energy mode statistics collection
+// <q SL_TRACING_ENERGY_STATS> Enable energy mode statistics collection
 // <i> Default: 0
-#define SILABS_TRACING_ENERGY_STATS 0
+#define SL_TRACING_ENERGY_STATS 0
 
-// <q SILABS_TRACING_ENERGY_TRACES_SECONDS> Time to wait before outputting the results
+// <q SL_TRACING_ENERGY_TRACES_SECONDS> Time to wait before outputting the results
 // <i> Default: 600
-#define SILABS_TRACING_ENERGY_TRACES_SECONDS 600
+#define SL_TRACING_ENERGY_TRACES_SECONDS 600
 
-// <q SILABS_TRACING_ENERGY_TRACES_MAX> Maximum number of energy traces to store
+// <q SL_TRACING_ENERGY_TRACES_MAX> Maximum number of energy traces to store
 // <i> Default: 3000
-#define SILABS_TRACING_ENERGY_TRACES_MAX 3000
+#define SL_TRACING_ENERGY_TRACES_MAX 3000
 
 // <<< end of configuration section >>>
 
