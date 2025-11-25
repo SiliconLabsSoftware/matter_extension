41a8a91f0e2de2eeb7321f9e12869a068c5aebf4
Michael Duggan <miduggan@silabs.com>
2025-11-25 09:27:57 -0500

    resolve timing issue with initializing second instance and cli

--- silabs_utils/init_matter_multipan.cpp
+++ silabs_utils/init_matter_multipan.cpp
@@ -6,8 +6,9 @@
 extern "C" {
 #include "platform-efr32.h"
 #if CHIP_DEVICE_CONFIG_THREAD_ENABLE_CLI
+#include "sl_ot_custom_cli.h"
 void otAppCliInit(otInstance * aInstance);
-#endif // CHIP_DEVICE_CONFIG_THREAD_ENABLE_CLI
+#endif // CHIP_DEVICE_CONFIG_THREAD_ENABLE_CLI  
 }
 
 // Global variable to store the OpenThread instance
@@ -15,13 +16,18 @@ static otInstance * myOtInstance = nullptr;
 
 /**
  * @brief Initialize Matter Multi-PAN support with internal app initialization
- * 
- * This function initializes a secondary OpenThread instance for Multi-PAN support
- * and moves the CLI to the proprietary Thread instance.
+ *
+ * This file initializes a secondary OpenThread instance for Multi-PAN support
+ * and initializes the OpenThread CLI to the secondary Thread instance.
  */
-extern "C" void sl_internal_app_init_matter_multipan()
+extern "C" void sl_internal_app_init_second_instance()
 {
     myOtInstance = otInstanceInitMultiple(1);
-    /* Move CLI to Proprietary Thread instance */
+}
+
+extern "C" void sl_internal_app_init_cli()
+{
+    /* Move CLI to the secondary Thread instance */
     otAppCliInit(myOtInstance);
+    sl_ot_custom_cli_init();
 }
