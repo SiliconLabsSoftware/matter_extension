fa54527856447a69fa831e134b9346574016b6e8
chirag-silabs <chirag.bansal@silabs.com>
2026-02-06 19:11:59 +0530

    removing the fixed zap version

--- slc/script/get_zap_version.py
+++ slc/script/get_zap_version.py
@@ -27,8 +27,7 @@ from pathlib import Path
 # This string should remain empty on the main branch. Update it only when a release branch is created. 
 # Coordinate with the studio team to determine the appropriate ZAP version for the release.
 # Version string example :  "v2024.08.14-nightly"
-# TODO: empty this once CSA is update, needed for TE2
-SIMPLICITY_STUDIO_ZAP_VERSION = "v2025.09.10-nightly"
+SIMPLICITY_STUDIO_ZAP_VERSION = ""
 
 def get_zap_version():
     if SIMPLICITY_STUDIO_ZAP_VERSION:
