93d476591bf1fd33e3f4d992957c5a8c4ef408c8
sarthak shaha <sarthak.shaha@silabs.com>
2026-05-01 13:32:00 -0400

    Update jenkinsFunctions.groovy

--- jenkins_integration/jenkinsFunctions.groovy
+++ jenkins_integration/jenkinsFunctions.groovy
@@ -403,7 +403,7 @@ def trigger_sqa_pipelines(pipeline_type)
 {
     if(sqaFunctions.isProductionJenkinsServer())
     {
-        def smoke_list = ['project_upgrades_lighting_app/smoke/matter']
+        def smoke_list = ['smoke']
         def regression_list = ['feature-thread', 'feature-wifi', 'feature-cmp', 'regression-thread', 'regression-wifi', 'regression-cmp',
                                'regression-ota-thread', 'regression-ota-wifi', 'regression-ota-cmp', 'regression-metrics',
                                'ext-regression-thread', 'ext-regression-wifi', 'ext-regression-cmp',
