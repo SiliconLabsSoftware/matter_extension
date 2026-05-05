252f338b2b64a33121eb9ce7ca05ca323a3f2278
Jean-Francois Penven <jean-francois.penven@silabs.com>
2026-05-05 10:41:18 -0400

    Fix comments

--- slc/inc/app/AppBuildConfig.h
+++ slc/inc/app/AppBuildConfig.h
@@ -11,7 +11,7 @@
 // <q CHIP_CONFIG_SUBSCRIPTION_TIMEOUT_RESUMPTION> Matter Subscription Timeout Resumption
 #define CHIP_CONFIG_SUBSCRIPTION_TIMEOUT_RESUMPTION 1
 
-// <i> Default: 1
+// <i> Default: 0
 // <q CHIP_CONFIG_ENABLE_READ_CLIENT> Read Client
 #define CHIP_CONFIG_ENABLE_READ_CLIENT 0
 
