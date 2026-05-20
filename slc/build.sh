5b14822883e0e7e7bb01fbd4cdd1c6340f226b9f
sarthak shaha <sarthak.shaha@silabs.com>
2026-05-20 16:03:40 -0400

    fix llvm (#741)
    * fix llvm
    
    * Update slc/build.sh
    
    Co-authored-by: lpbeliveau-silabs <112982107+lpbeliveau-silabs@users.noreply.github.com>
    
    * remove comment
    
    ---------
    
    Co-authored-by: lpbeliveau-silabs <112982107+lpbeliveau-silabs@users.noreply.github.com>

--- slc/build.sh
+++ slc/build.sh
@@ -449,7 +449,9 @@ elif [ "$GENERATE_BOOTLOADER" = false ] && [ "$GENERATE_APPLICATION" = true ]; t
 else
 	echo "Building solution..."
 	if [ "$USE_LLVM" = true ]; then
-		cmake_configure_and_build "$OUTPUT_DIR/$CMAKE_SUBDIR" "solution" || exit 1
+		# Note: When slc-cli 6.0.21 releases, need to revert back to: 
+		# cmake_configure_and_build "$OUTPUT_DIR/$CMAKE_SUBDIR" "solution" || exit 1 
+		cmake_configure_and_build "$OUTPUT_DIR/${SILABS_APP}_llvm_cmake" "solution" || exit 1        
 	else
 		if ! make all -C "$OUTPUT_DIR" -f "$MAKE_FILE" -j13; then
 			echo "ERROR: Failed to build solution"
