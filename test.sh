de657e572aefc7ffc0391eabfde169d40d7b58a6
Rohan Sahay <103027015+rosahay-silabs@users.noreply.github.com>
2026-09-01 10:58:59 +0530

    MATTER:7023 - Changes as per feedback on Alpha1 (#1017)
    * Update matter_sdk pointer
    
    * Enhance test.sh to include application size reporting and adjust RTT connection timeout
    
    * Update test.sh to include source copying during build
    
    * TEMP: fix for this diff of wifi_sdk
    
    * Update matter_sdk pointer

--- test.sh
+++ test.sh
@@ -5,9 +5,16 @@ GENERATE=false
 if [ ! -d "out" ]; then
   GENERATE=true
 fi
+
 echo "BUILDING..."
-./slc/build.sh ./slc/apps/thermostat/wifi/matter_wifi_soc_thermostat_dual_stack_freertos.slcw "brd4338a" $([ "$GENERATE" = false ] && echo --skip_gen) 2>&1 > build.log
+./slc/build.sh ./slc/apps/thermostat/wifi/matter_wifi_soc_thermostat_dual_stack_freertos.slcw "brd4338a" --copy-sources $([ "$GENERATE" = false ] && echo --skip_gen) 2>&1 > build.log
 tail build.log
+
+echo "SIZE..."
+OUT_APP=out/brd4338a/matter_wifi_soc_thermostat_dual_stack_freertos_solution/thermostat_siwx/build/debug/matter_wifi_soc_thermostat_dual_stack_freertos.out
+ARM_GCC_DIR="$(./slc/tools/slt where gcc-arm-none-eabi)"
+$ARM_GCC_DIR/bin/arm-none-eabi-size -A "${OUT_APP}" | tee -a build.log
+
 echo "FLASHING..."
 commander rps load out/brd4338a/matter_wifi_soc_thermostat_dual_stack_freertos_solution/artifact/matter_wifi_soc_thermostat_dual_stack_freertos.rps
 sleep 5
@@ -18,5 +25,4 @@ if [ "$(uname)" == "Darwin" ]; then
   TIMEOUT_CMD=gtimeout
 fi
 echo "CONNECTING..."
-$TIMEOUT_CMD 180 commander rtt connect 2>&1 |tee device.log
-
+$TIMEOUT_CMD 120 commander rtt connect 2>&1 |tee device.log
