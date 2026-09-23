575e8c38eb70eaa998f763eccc549eca1dc24ad1
Sarthak Shaha <sarthak.shaha@silabs.com>
2026-09-23 13:47:14 -0400

    revert unneccessary changes

--- slc/config/sl_matter_icd_config.h
+++ slc/config/sl_matter_icd_config.h
@@ -14,7 +14,6 @@
 // <o SL_SHORT_IDLE_MODE_DURATION_S>  Short Idle Mode Duration (sec)
 // <i> A shorter idle mode duration when a LIT capable device is operating as a SIT
 // <i> Default value is to use the same value as SL_IDLE_MODE_DURATION_S
-// <i> Default: 600 (10 minutes)
 #define SL_SHORT_IDLE_MODE_DURATION_S SL_IDLE_MODE_DURATION_S
 
 // <o SL_ACTIVE_MODE_DURATION_MS>  Active Mode Duration (ms)
