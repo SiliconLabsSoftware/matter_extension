197c2625e9b6901051c2b2b3b2fc2b710c9449bb
Sarthak Shaha <sarthak.shaha@silabs.com>
2026-09-24 11:07:34 -0400

    revert unneccessary changes

--- slc/config/sl_matter_icd_config.h
+++ slc/config/sl_matter_icd_config.h
@@ -14,7 +14,6 @@
 // <o SL_SHORT_IDLE_MODE_DURATION_S>  Short Idle Mode Duration (sec)
 // <i> A shorter idle mode duration when a LIT capable device is operating as a SIT
 // <i> Default value is to use the same value as SL_IDLE_MODE_DURATION_S
-// <i> Default: 600 (10 minutes)
 #define SL_SHORT_IDLE_MODE_DURATION_S SL_IDLE_MODE_DURATION_S
 
 // <o SL_ACTIVE_MODE_DURATION_MS>  Active Mode Duration (ms)
@@ -63,14 +62,5 @@
 #define SL_ICD_ENABLE_SELECTIVE_SLEEP 1
 
 // <<< end of configuration section >>>
-
-// OpenThread overrides to align child timeouts with the Matter ICD idle duration.
-// Included by OpenThread through SL_OPENTHREAD_APPLICATION_CONFIG_FILE.
-
-// MLE child timeout (sec): two missed idle periods plus one second.
-#define OPENTHREAD_CONFIG_MLE_CHILD_TIMEOUT_DEFAULT ((SL_IDLE_MODE_DURATION_S * 2) + 1)
-
-// Child supervision check timeout (sec): one idle period, so it cannot wake the device mid-idle.
-#define OPENTHREAD_CONFIG_CHILD_SUPERVISION_CHECK_TIMEOUT SL_IDLE_MODE_DURATION_S
 
 #endif // SL_MATTER_ICD_CONFIG_H
